100c9c19e31f558b32dab20837e755ff048b83f9
SOOYEON YOO <dbtndus0527@gmail.com>
2023-02-09 10:37:19 +0900

    [Bronze V] Title: A+B, Time: 208 ms, Memory: 17768 KB -BaekjoonHub

---
+++ 백준/Bronze/1000. A＋B/A＋B.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner s = new Scanner(System.in);
+		int a = s.nextInt();
+		int b = s.nextInt();
+		int res = a+b;
+		System.out.println(a+b);
+
+	}
+
+}

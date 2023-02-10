ec676529f054e6208bf4a4d31d45cf9b9bd28973
SOOYEON YOO <dbtndus0527@gmail.com>
2023-02-10 17:35:37 +0900

    [Silver IV] Title: 듣보잡, Time: 96 ms, Memory: 43472 KB -BaekjoonHub

--- 백준/Silver/1764. 듣보잡/듣보잡.py
+++ 백준/Silver/1764. 듣보잡/듣보잡.py
@@ -1,13 +1,15 @@
+import sys
+
 arr1 = []
 arr2 = []
 
 N, M = map(int, input().split())
 
 for i in range(N):
-    arr1.append(input())
+    arr1.append(sys.stdin.readline())
     
 for i in range(M):
-    arr2.append(input())
+    arr2.append(sys.stdin.readline())
 
 arr1 = set(arr1)
 arr2 = set(arr2)
@@ -17,4 +19,4 @@ ans.sort()
 
 print(len(ans))
 for name in ans:
-    print(name)
+    print(name, end='')

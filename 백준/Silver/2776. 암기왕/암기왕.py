8c84f67a0d86eed0e595cb4fccfb51383dbb421e
SOOYEON YOO <dbtndus0527@gmail.com>
2023-02-15 12:24:59 +0900

    [Silver IV] Title: 암기왕, Time: 5372 ms, Memory: 187320 KB -BaekjoonHub

--- 백준/Silver/2776. 암기왕/암기왕.py
+++ 백준/Silver/2776. 암기왕/암기왕.py
@@ -1,3 +1,5 @@
+import sys
+
 def binary_search(num):
     start, end = 0, len(arr1) - 1
     while(start <= end):
@@ -18,11 +20,11 @@ i = 0
 while(i < case):
 
     N = int(input())
-    arr1 = list(map(int, input().split()))
+    arr1 = list(map(int, sys.stdin.readline().split()))
     arr1.sort()
 
     M = int(input())
-    arr2 = list(map(int, input().split()))
+    arr2 = list(map(int, sys.stdin.readline().split()))
     
     for n in arr2:
         print(binary_search(n))

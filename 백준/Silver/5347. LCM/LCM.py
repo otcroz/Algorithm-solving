41dad776cad317bde33049f43705f4d9af139e38
otcroz <otcroz.dev@gmail.com>
2024-09-04 11:54:56 +0900

    [Silver V] Title: LCM, Time: 28 ms, Memory: 33240 KB -BaekjoonHub

--- 백준/Silver/5347. LCM/LCM.py
+++ 백준/Silver/5347. LCM/LCM.py
@@ -1,16 +1,10 @@
+from math import gcd
+
+def lcm(n1, n2):
+    return  n1*n2 // gcd(n1, n2)
+
 n = int(input())
-
-def gcd(n1, n2): 
-    while (n2 != 0):
-        n1, n2 = n2, n1 % n2
-    
-    return n1
-
 
 for _ in range(n):
     a, b = map(int, input().split())
-
-    if a > b:
-        print((a*b) // gcd(a, b))
-    else:
-        print((a*b) // gcd(b, a))
+    print(lcm(a, b))

667796928d72101f19f491f53c7312ab232979b6
otcroz <otcroz.dev@gmail.com>
2024-09-25 14:20:23 +0900

    [Silver IV] Title: 카드2, Time: 340 ms, Memory: 50940 KB -BaekjoonHub

--- 백준/Silver/2164. 카드2/카드2.py
+++ 백준/Silver/2164. 카드2/카드2.py
@@ -1,19 +1,19 @@
 from collections import deque
+import sys
 
-dq = deque() # deque 생성
+input = sys.stdin.readline
+dq = deque()
+cnt = 0
 
 N = int(input())
+for i in range(N):
+    dq.append(i+1)
 
-for i in range(1, N+1):
-    dq.appendleft(i) # 왼쪽에 queue 추가
-
-i = 0
-while(len(dq) >1):
-    if i % 2 == 0:
-        dq.pop()
+while(len(dq) != 1):
+    if cnt % 2 == 0:
+        dq.popleft()
     else:
-        dq.rotate(1) # queue 회전
-        
-    i+=1
-
+        temp = dq.popleft()
+        dq.append(temp)
+    cnt += 1
 print(dq[0])

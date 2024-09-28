6eeb1981bb4b7207e692622dba108166d6b97c37
otcroz <otcroz.dev@gmail.com>
2024-09-28 18:49:05 +0900

    [Silver I] Title: 미로 탐색, Time: 84 ms, Memory: 34072 KB -BaekjoonHub

--- 백준/Silver/2178. 미로 탐색/미로 탐색.py
+++ 백준/Silver/2178. 미로 탐색/미로 탐색.py
@@ -1,24 +1,27 @@
 from collections import deque
+import sys
+input = sys.stdin.readline
+
+arr = []
+nx = [-1, 1, 0 ,0]
+ny = [0, 0, -1, 1]
+dq = deque([(0, 0)])
 
 n, m = map(int, input().split())
-maze = []
-dx = [-1, 1, 0, 0]
-dy = [0, 0, -1, 1]
 
-for i in range(n):
-  maze.append(list(map(int, input())))
+for _ in range(n):
+    arr.append(list(map(int, input().rstrip('\n'))))
 
-queue = deque([(0,0)])
+while(dq):
+    x, y = dq.popleft()
 
-while queue:
-  x, y = queue.popleft()
-  for i in range(4):
-    nx = x + dx[i]
-    ny = y + dy[i]
+    for i in range(4):
+        dx = x + nx[i]
+        dy = y + ny[i]
 
-    if 0 <= nx < n and 0 <= ny < m:
-      if maze[nx][ny] == 1:
-        queue.append([nx, ny])
-        maze[nx][ny] = maze[x][y] + 1
+        if dx >= 0 and dx < n and dy >=0 and dy < m:
+            if arr[dx][dy] == 1: 
+                dq.append([dx, dy])
+                arr[dx][dy] = arr[x][y] + 1
 
-print(maze[n-1][m-1])
+print(arr[n-1][m-1])

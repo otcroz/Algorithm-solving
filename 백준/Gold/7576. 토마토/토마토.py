dfea39add1718e829afa0e35286aefac9e8bc5fa
otcroz <otcroz.dev@gmail.com>
2024-09-28 19:19:00 +0900

    [Gold V] Title: 토마토, Time: 1276 ms, Memory: 106788 KB -BaekjoonHub

--- 백준/Gold/7576. 토마토/토마토.py
+++ 백준/Gold/7576. 토마토/토마토.py
@@ -1,44 +1,45 @@
 from collections import deque
-
-w, h = map(int, input().split(' '))
-
-dx = [0, 0, -1, 1]
-dy = [-1, 1, 0, 0]
-day = 1
-queue = deque([])
+import sys
+input = sys.stdin.readline
 
 arr = []
-for i in range(h):
-    temp = list(map(int, input().split(' ')))
-    arr.append(temp)
+day = 1
+nx = [0, 0, -1, 1]
+ny = [1, -1, 0, 0]
+dq = deque([])
 
 def bfs():
     global day
-    while queue:
-        bh, bw = queue.popleft()
+    
+    while dq:
+        x, y = dq.popleft()
         for i in range(4):
-            nx = dx[i] + bw
-            ny = dy[i] + bh
+            dx = x + nx[i]
+            dy = y + ny[i]
 
-            if nx >= 0 and nx < w and ny >= 0 and ny < h:
-                if arr[ny][nx] == 0:
-                    arr[ny][nx] = arr[bh][bw] + 1
-                    day = arr[ny][nx]
-                    queue.append([ny, nx])
-                    
+            if dx >= 0 and dx < m and dy >=0 and dy < n:
+                if arr[dx][dy] == 0:
+                    arr[dx][dy] = arr[x][y] + 1
+                    day = arr[dx][dy]
+                    dq.append([dx, dy])
 
+n, m = map(int, input().split())
 
-for i in range(h):
-    for j in range(w):
+for _ in range(m):
+    arr.append(list(map(int, input().split())))
+
+for i in range(m):
+    for j in range(n):
         if arr[i][j] == 1:
-            queue.append([i, j])
+            dq.append([i, j])
 
 bfs()
 
-for i in range(h):
-    for j in range(w):
+# check
+for i in range(m):
+    for j in range(n):
         if arr[i][j] == 0:
             print(-1)
             exit(0)
 
-print(day - 1)
+print(day-1)

2321c83c62953830fd9b74c7e71e6e9df11d6489
otcroz <otcroz.dev@gmail.com>
2024-10-01 03:01:03 +0900

    [Gold V] Title: 적록색약, Time: 60 ms, Memory: 31564 KB -BaekjoonHub

--- 백준/Gold/10026. 적록색약/적록색약.py
+++ 백준/Gold/10026. 적록색약/적록색약.py
@@ -1,52 +1,56 @@
 import sys
-sys.setrecursionlimit(1000000)
+sys.setrecursionlimit(100000)
+input = sys.stdin.readline
+
+arr = []
+res = []
+cnt = 0
+
+dx = [0, 0, -1, 1]
+dy = [-1, 1, 0, 0]
 
 N = int(input())
 
-arr = []
-for i in range(N):
-    arr.append(list(map(str, input())))
 visited = [[0] * N for _ in range(N)]
 
-dy = [-1, 1, 0, 0]
-dx = [-0, 0, -1, 1]
-    
-two_cnt = 0
-three_cnt = 0
+def dfs(a, b):
+    cur_c = arr[a][b]
 
-def dfs(a, b):    
-    visited[a][b] = 1
-    current_color = arr[a][b]
-    
     for i in range(4):
-        ny = dy[i] + b
-        nx = dx[i] + a
+        nx = a + dx[i]
+        ny = b + dy[i]
 
-        if(0 <= nx < N) and (0 <= ny < N):
-            if visited[nx][ny] == 0:
-                if arr[nx][ny] == current_color:
-                    dfs(nx, ny)
-# 색약 x
+        if nx >= 0 and nx < N and ny >=0 and ny < N:
+            if visited[nx][ny] == 0 and arr[nx][ny] == cur_c:
+                visited[nx][ny] = 1
+                dfs(nx, ny)
+                    
+
+arr = [list(map(str, input().rstrip('\n'))) for _ in range(N)]
+
 for i in range(N):
     for j in range(N):
         if visited[i][j] == 0:
             dfs(i, j)
-            three_cnt += 1
-            
-                
+            cnt += 1
+
+res.append(cnt)
+
+# 적록 색약의 경우
 for i in range(N):
     for j in range(N):
         if arr[i][j] == 'R':
             arr[i][j] = 'G'
-            
-#visited = [[0]*N]*N
+
+cnt = 0
 visited = [[0] * N for _ in range(N)]
 
-# 색약
 for i in range(N):
     for j in range(N):
         if visited[i][j] == 0:
             dfs(i, j)
-            two_cnt += 1
-            
-print(three_cnt, two_cnt)
+            cnt += 1
+
+res.append(cnt)
+
+print(res[0], res[1])

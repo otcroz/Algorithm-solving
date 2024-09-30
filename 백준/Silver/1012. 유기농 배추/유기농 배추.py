c065921f1e07442f3c46c90bfb04d38521c6fbab
otcroz <otcroz.dev@gmail.com>
2024-10-01 02:16:28 +0900

    [Silver II] Title: 유기농 배추, Time: 60 ms, Memory: 34096 KB -BaekjoonHub

--- 백준/Silver/1012. 유기농 배추/유기농 배추.py
+++ 백준/Silver/1012. 유기농 배추/유기농 배추.py
@@ -1,49 +1,43 @@
+import sys
 from collections import deque
+input = sys.stdin.readline
 
-dx = [-1,1,0,0]
-dy = [0,0,-1,1]
+cnt = 0
+arr = []
+dx = [0, 0, -1, 1]
+dy = [1, -1, 0, 0]
 
 T = int(input())
-link = 0
 
 def bfs(a, b):
-  Q = deque([(a, b)])
-  while Q:
-    x, y = Q.popleft()
-  
-    for i in range(4):
-      nx = x + dx[i]
-      ny = y + dy[i]
-  
-      if 0 <= nx < N and 0 <= ny < M:
-        if graph[nx][ny] == 1:
-          graph[nx][ny] = 0
-          Q.append([nx, ny])
-        
+    global cnt
+    dq = deque([(a, b)])
+    cnt += 1
 
-test_count = 0
-while test_count < T:
-  M, N, K = map(int, input().split())
-  graph = [[] for _ in range(N)]
-  
-  # 초기화
-  for i in range(N):
-    for k in range(M):
-      graph[i].append(int('0')) 
-  
-  # 배추의 위치
-  for i in range(K):
-    a, b = map(int, input().split())
-    graph[b][a] = 1
-  
-  # 탐색
-  for i in range(N):
-    for j in range(M):
-      if graph[i][j] == 1:
-        link += 1
-        graph[i][j] = 0
-        bfs(i, j)
-     
-  print(link)
-  link = 0
-  test_count += 1
+    while dq:
+        a, b = dq.popleft()
+
+        for i in range(4):
+            nx = a + dx[i]
+            ny = b + dy[i]
+
+            if (nx >= 0 and nx < m and ny >= 0 and ny < n):
+                if arr[nx][ny] == 1:
+                    arr[nx][ny] = 0
+                    dq.append([nx, ny])
+
+for _ in range(T):
+    n, m, k = map(int, input().split())
+    arr = [[0] * n for _ in range(m)]
+
+    for i in range(k):
+        x, y = map(int, input().split())
+        arr[y][x] = 1
+
+    for i in range(m):
+        for j in range(n):
+            if arr[i][j] == 1:
+                bfs(i, j)
+    print(cnt)
+    cnt = 0
+    arr = []

ae8a3712c8a28b39a59dbb7660ce3e6e0d728900
otcroz <otcroz.dev@gmail.com>
2024-10-17 13:03:37 +0900

    [Bronze II] Title: 분해합, Time: 1204 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/2231. 분해합/분해합.py
+++ 백준/Bronze/2231. 분해합/분해합.py
@@ -5,7 +5,7 @@ for i in range(N, 0, -1): # 거꾸로
     sub = N - i
     sub_sum = sum(list(map(int, str(i))))
     if sub == sub_sum:
-        if min > sub_sum:
+        if min > i:
             min = i
 if min == 1000000:
     min = 0

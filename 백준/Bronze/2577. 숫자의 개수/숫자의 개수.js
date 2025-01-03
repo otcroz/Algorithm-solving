934faf2e7f4c053bf87384fb1dccdb8002cd67fb
otcroz <otcroz.dev@gmail.com>
2025-01-04 02:01:34 +0900

    [Bronze II] Title: 숫자의 개수, Time: 92 ms, Memory: 9580 KB -BaekjoonHub

--- 백준/Bronze/2577. 숫자의 개수/숫자의 개수.js
+++ 백준/Bronze/2577. 숫자의 개수/숫자의 개수.js
@@ -1,7 +1,10 @@
-const input = require('fs').readFileSync(process.platform == 'win32' ? "./input.txt" : '/dev/stdin').toString().split("\n").map(Number);
+const input = require('fs').readFileSync(process.platform == 'win32' ? "./input.txt" : '/dev/stdin').toString().trim().split("\n").map(Number);
 
 const solution = () => {
-    const calc = (input[0] * input[1] * input[2]).toString();
+    const calc = input.reduce((acc, cur) => 
+        acc * Number(cur)
+    , 1).toString();
+    //const calc = (input[0] * input[1] * input[2]).toString();
     const arr = Array(10).fill(0);
 
     for (num of calc) { 

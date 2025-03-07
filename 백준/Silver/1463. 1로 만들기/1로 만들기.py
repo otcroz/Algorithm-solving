c896ee10a22226ea5672ecd0ea0ae66d1e4697e5
otcroz <otcroz.dev@gmail.com>
2025-03-07 18:44:17 +0900

    [Silver III] Title: 1로 만들기, Time: 464 ms, Memory: 40224 KB -BaekjoonHub

--- 백준/Silver/1463. 1로 만들기/1로 만들기.py
+++ 백준/Silver/1463. 1로 만들기/1로 만들기.py
@@ -1,13 +1,12 @@
-X = int(input())
-
+N = int(input())
 d = [0] * 1000001
 
-for i in range(2, X+1):
-    d[i] = d[i-1] + 1 # 1을 빼줌
+for i in range(2, N+1):
+    d[i] = 1 + d[i-1]
+    
+    if i%3 == 0:
+        d[i] = min(d[i], d[i//3]+1)
+    if i%2 == 0:
+        d[i] = min(d[i], d[i//2]+1)
 
-    if i % 3 == 0:
-        d[i] = min(d[i], d[i//3] + 1)
-    if i % 2 == 0:
-        d[i] = min(d[i], d[i//2] + 1)
-
-print(d[X])
+print(d[N])

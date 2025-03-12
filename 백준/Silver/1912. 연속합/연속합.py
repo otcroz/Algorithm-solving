0e59a8ba065d3878a19e361266bfb2a9c2381410
otcroz <otcroz.dev@gmail.com>
2025-03-12 15:01:26 +0900

    [Silver II] Title: 연속합, Time: 80 ms, Memory: 39096 KB -BaekjoonHub

--- 백준/Silver/1912. 연속합/연속합.py
+++ 백준/Silver/1912. 연속합/연속합.py
@@ -1,13 +1,8 @@
 N = int(input())
 
 arr = list(map(int, input().split()))
-dp = [0] * 100000
-
-dp[0] = arr[0]
-result = arr[0]
 
 for i in range(1, N):
-    dp[i] = max(arr[i], arr[i] + dp[i-1])
-    result = max(dp[i], result)
+    arr[i] = max(arr[i], arr[i] + arr[i-1])
     
-print(result)
+print(max(arr))

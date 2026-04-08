f268f525fd04e0be4167ba6f5a8a1d7e3a9ad5ff
otcroz <otcroz.dev@gmail.com>
2026-04-08 21:24:41 +0900

    [Silver III] Title: N과 M (7), Time: 1264 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/15656. N과 M （7）/N과 M （7）.py
+++ 백준/Silver/15656. N과 M （7）/N과 M （7）.py
@@ -1,18 +1,16 @@
 n, m = map(int, input().split())
 arr = list(map(int, input().split()))
-
 arr.sort()
-s = []
-v = [0] * n
+res = []
 
 def back():
-    if len(s) == m:
-        print(' '.join(map(str, s)))
-        return
-    for i in range(n):
-        v[i] = 1
-        s.append(arr[i])
-        back()
-        s.pop()
-        v[i] = 0
+  if len(res) == m:
+    print(' '.join(map(str, res)))
+    return
+  
+  for i in range(len(arr)):
+    res.append(arr[i])
+    back()
+    res.pop()
+
 back()

de54e8903ce037b0dba97cc3e3c44fb113fa46da
otcroz <otcroz.dev@gmail.com>
2026-04-08 21:21:08 +0900

    [Silver III] Title: N과 M (5), Time: 164 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/15654. N과 M （5）/N과 M （5）.py
+++ 백준/Silver/15654. N과 M （5）/N과 M （5）.py
@@ -1,19 +1,17 @@
 n, m = map(int, input().split())
 arr = list(map(int, input().split()))
-arr.sort() #O(n)
-v = [0] * n
-
+arr.sort()
 res = []
-def dfs():
-    if len(res) == m:
-        print(" ".join(map(str, res)))
-        return
-    for j in range(n):
-        if v[j] == 1:
-            continue
-        res.append(arr[j])
-        v[j] = 1
-        dfs()
+
+def back(k):
+  if len(res) == m:
+    print(' '.join(map(str, res)))
+    return
+  
+  for i in range(len(arr)):
+      if arr[i] not in res:
+        res.append(arr[i])
+        back(i+1)
         res.pop()
-        v[j] = 0
-dfs()
+
+back(0)

a4fec3bb04c432135fd1c288e7c465c90fa99568
otcroz <otcroz.dev@gmail.com>
2026-04-08 21:45:40 +0900

    [Silver II] Title: N과 M (9), Time: 124 ms, Memory: 37024 KB -BaekjoonHub

--- 백준/Silver/15663. N과 M （9）/N과 M （9）.py
+++ 백준/Silver/15663. N과 M （9）/N과 M （9）.py
@@ -1,26 +1,25 @@
-import sys
-input = sys.stdin.readline
-
-n,m = map(int, input().split())
+n, m = map(int, input().split())
 arr = list(map(int, input().split()))
 arr.sort()
-v = [0] * len(arr)
-
 res = []
 res_comb = set()
+v = [False]*n
+
 def back(res_comb):
-    if len(res) == m:
-        res_tuple = tuple(res)
-        if res_tuple not in res_comb: #동일한 조합이 없는 경우에만
-            res_comb.add(res_tuple)
-        return
-    for i in range(len(arr)):
-        if v[i] == 0:
-            v[i] = 1
-            res.append(arr[i])
-            back(res_comb)
-            res.pop()
-            v[i] = 0
+  if len(res) == m:
+    res_tuple = tuple(res)
+    if res_tuple not in res_comb:
+      res_comb.add(res_tuple)
+      return
+  
+  for i in range(len(arr)):
+    if not v[i]:
+      v[i] = True
+      res.append(arr[i])
+      back(res_comb)
+      res.pop()
+      v[i]= False
+
 back(res_comb)
 for res in sorted(res_comb):
-    print(' '.join(map(str, res)))
+  print(' '.join(map(str, res)))

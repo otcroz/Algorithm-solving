110240003522c9d13576bd881caf2aad943015c6
otcroz <otcroz.dev@gmail.com>
2026-04-27 05:55:10 +0900

    [Silver III] Title: 1, 2, 3 더하기, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/9095. 1， 2， 3 더하기/1， 2， 3 더하기.py
+++ 백준/Silver/9095. 1， 2， 3 더하기/1， 2， 3 더하기.py
@@ -1,13 +1,10 @@
-T = int(input())
+t = int(input())
 
-d = [0] * 11
-d[1] = 1
-d[2] = 2
-d[3] = 4
-
-for i in range(4, 11):
+d = [0] * 12
+d[1] = 1; d[2] = 2; d[3] = 4
+for i in range(4, 12):
     d[i] = d[i-1] + d[i-2] + d[i-3]
 
-for _ in range(T):
-    N = int(input())
-    print(d[N])
+for _ in range(t):
+    n = int(input())
+    print(d[n])
